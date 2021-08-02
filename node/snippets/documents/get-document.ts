ba5412e2c444eeb9096e9cc8817e8f78a2a3bdcb
davemooreuws <davemooreuws@gmail.com>
2021-08-02 16:32:12 +1000

    refactor: fix get document test node

--- node/snippets/documents/get-document.ts
+++ node/snippets/documents/get-document.ts
@@ -6,8 +6,19 @@ import { documents } from '@nitric/sdk';
 
 const proto = grpc.document.DocumentServiceClient.prototype;
 
+const MOCKED_GET_RESPONSE = {
+  hasDocument: () => true,
+  getDocument: () => ({
+    getContent: () => ({
+      toJavaScript: () => true,
+    }),
+  }),
+};
+
 test('Should get a documents content', async () => {
-  jest.spyOn(proto, 'get').mockImplementation((_, cb: any) => cb(null, true));
+  jest
+    .spyOn(proto, 'get')
+    .mockImplementation((_, cb: any) => cb(null, MOCKED_GET_RESPONSE));
 
   // [START snippet]
   const document = documents().collection('products').doc('nitric');

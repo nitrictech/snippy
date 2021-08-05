3744f55d2734fa7e4dbc7aaf5b2de4e35a51a8cb
davemooreuws <davemooreuws@gmail.com>
2021-08-05 12:32:22 +1000

    chore: add license and more workflows

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,16 @@
+// Copyright 2021, Nitric Technologies Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // Regex for [START] and [END] snippet tags.
 export const START_TAG_REGEX = /\[START\s+([A-Za-z_]+)\s*\]/;
 export const END_TAG_REGEX = /\[END\s+([A-Za-z_]+)\s*\]/;
